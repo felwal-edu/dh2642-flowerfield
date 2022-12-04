10f2d4ee67ae16806686f34d9c070d02797e7422
Felix Wallin <nils.felix@gmail.com>
2022-12-04 19:30:23 +0100

    Firebase: set up auth

--- src/data/persistence/firebaseModel.js
+++ src/data/persistence/firebaseModel.js
@@ -1,17 +1,28 @@
 import firebaseConfig from "./firebaseSecrets";
 import { initializeApp } from "firebase/app";
 import { getDatabase, ref, set } from "firebase/database";
+import { examplePlant } from "../network/plantIdServiceMock";
+
+// init
 
 //console.log("initializing firebase ...")
 
-// Initialize firebase
-const firebase = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
 const db = getDatabase();
 
+const REF = "flowerModel";
+
 // (temp) check that the database is correctly initialized
-set(ref(db, "/test"), "dummy");
+set(ref(db, REF + "/test"), "dummy");
+set(ref(db, REF + "/users/0"), "test");
 
 //
+
+export function createUser(user) {
+  // TODO: temp
+  set(ref(db, REF + "/users/" + user.uid + "/email"), user.email);
+  set(ref(db, REF + "/users/" + user.uid + "/plants"), [examplePlant]);
+}
 
 export function updateFirebaseFromModel(model) {
   function observeACB(payload) {
---
+++ src/data/persistence/firebaseTest.js
@@ -0,0 +1,73 @@
+import resolvePromise from "../network/resolvePromise";
+import { observeAuthState, signInUser, signOutUser, signUpUser } from "./firebaseAuth";
+import { createUser } from "./firebaseModel";
+
+// TODO: move to loginPresenter
+
+const promiseState = {}
+
+let currentUser = undefined;
+
+//
+
+function signedInACB(user) {
+  console.log(user);
+
+  currentUser = user;
+
+  // TODO: create firebase entry if first time
+  createUser(user);
+
+  // TODO: load data firebase -> model
+}
+
+function signedOutACB() {
+  currentUser = null;
+
+  // TODO: empty model
+}
+
+function authResultACB() {
+  console.log("promise state:");
+  console.log(promiseState);
+
+  // we probably dont need to do anything here
+}
+
+//
+
+function signUp(email, password) {
+  resolvePromise(signUpUser(email, password), promiseState, authResultACB);
+}
+
+function signIn(email, password) {
+  resolvePromise(signInUser(email, password), promiseState, authResultACB);
+}
+
+//
+
+function signUpClickACB(evt) {
+  if (currentUser !== null || currentUser === undefined) return;
+
+  signUp("nils.felix@gmail.com", "Hejhej1");
+}
+
+function signInClickACB(evt) {
+  if (currentUser !== null || currentUser === undefined) return;
+
+  signIn("nils.felix@gmail.com", "Hejhej1");
+}
+
+function signOutClickACB(evt) {
+  if (!currentUser) return;
+
+  signOutUser();
+}
+
+//
+
+observeAuthState(signedInACB, signedOutACB);
+
+//signUpClickACB();
+//signInClickACB();
+//signOutClickACB();

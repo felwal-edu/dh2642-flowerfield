be6c44267ba2b996ecbf1387e20aa9aa184c3884
Elliot <darthjr69@gmail.com>
2022-12-05 10:14:58 +0100

    Corrected Indentations

--- src/components/views/uploadView.js
+++ src/components/views/uploadView.js
@@ -14,23 +14,23 @@ function UploadView(props) {
 
     return (
         <div class="upload-page">
-        <div class="upload-container">
-          <h3>Upload your flower image</h3>
-          <div class="drag-area">
-            <div class="icon">
-              <i class="fas"></i>
+            <div class="upload-container">
+                <h3>Upload your flower image</h3>
+                <div class="drag-area">
+                    <div class="icon">
+                    <i class="fas"></i>
+                    </div>
+                    <span class="header">Drag & Drop</span>
+                    <span class="header">or <span class="browse-button" onClick={browseSpanClickACB}>browse</span></span>
+                    <input type="file" hidden />
+                    <span class="tip-support">PNG, JPG, JPEG</span>
+                </div>
             </div>
-            <span class="header">Drag & Drop</span>
-            <span class="header">or <span class="browse-button" onClick={browseSpanClickACB}>browse</span></span>
-            <input type="file" hidden />
-            <span class="tip-support">PNG, JPG, JPEG</span>
-          </div>
+            <div class="buttons-container">
+                <button class="btn upload" hidden onClick={uploadButtonClickACB}>Upload</button>
+                <button class="btn cancle" hidden onClick={cancleButtonClickACB}>Cancle</button>
+            </div>
         </div>
-        <div class="buttons-container">
-          <button class="btn upload" hidden onClick={uploadButtonClickACB}>Upload</button>
-          <button class="btn cancle" hidden onClick={cancleButtonClickACB}>Cancle</button>
-        </div>
-      </div>
     );
   }
 

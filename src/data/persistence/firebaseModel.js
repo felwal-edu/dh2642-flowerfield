f549793a08b00ecbd92873b00fd0cfca7bf4337d
Felix Wallin <nils.felix@gmail.com>
2022-12-05 21:44:41 +0100

    Firebase: sync with Pinia

--- src/data/persistence/firebaseModel.js
+++ src/data/persistence/firebaseModel.js
@@ -1,6 +1,6 @@
 import firebaseConfig from "./firebaseSecrets";
 import { initializeApp } from "firebase/app";
-import { getDatabase, ref, set, onValue, onChildAdded, onChildRemoved } from "firebase/database";
+import { getDatabase, ref, set, onChildAdded, onChildRemoved, get, child } from "firebase/database";
 
 // init
 
@@ -11,41 +11,93 @@ const db = getDatabase();
 
 const REF = "flowerModel";
 
+let unsubscribers = [];
+
 //
 
-export function updateUserData(user) {
-  // TODO: temp?
+export function createUser(user) {
   set(ref(db, REF + "/users/" + user.uid + "/email"), user.email);
 }
 
-export function updateFirebaseFromModel(model, uid) {
-  function observeACB(payload) {
-    if (!payload) return;
+export function updateFirebaseFromStore(store) {
+  function dataChangedInStoreACB(mutation, state) {
+    // check if nothng has changed
+    if (!mutation.events) return;
 
-    if (payload.addPlant) {
-      set(ref(db, REF + "/users/" + uid + "/plants/" + payload.addPlant.id), payload.addPlant);
+    function toIdKeyedObjectCB(obj, plant) {
+      return {...obj, [plant.id]: plant};
     }
-    else if (payload.removePlantId) {
-      set(ref(db, REF + "/users/" + uid + "/plants/" + payload.removePlantId), null);
+
+    if (mutation.events.key === "plants") {
+      // transform plant list to object with id as key
+      const plantsObj = mutation.events.newValue.reduce(toIdKeyedObjectCB, {});
+      set(ref(db, REF + "/users/" + store.currentUser.uid + "/plants/"), plantsObj);
     }
   }
 
-  model.addObserver(observeACB);
+  unsubscribers = [
+    ...unsubscribers,
+    store.$subscribe(dataChangedInStoreACB)
+  ];
 
   return;
 }
 
-export function updateModelFromFirebase(model, uid) {
+export function updateStoreFromFirebase(store) {
   function plantAddedInFirebase(data) {
-    model.addPlant(data.val());
+    store.addPlant(data.val());
   }
 
   function plantRemovedInFirebase(data) {
-    model.removePlant(+data.key);
+    store.removePlant(+data.key);
   }
 
-  onChildAdded(ref(db, REF + "/users/" + uid + "/plants"), plantAddedInFirebase);
-  onChildRemoved(ref(db, REF + "/users/" + uid + "/plants"), plantRemovedInFirebase);
+  unsubscribers = [
+    ...unsubscribers,
+    onChildAdded(ref(db, REF + "/users/" + store.currentUser.uid + "/plants"), plantAddedInFirebase),
+    onChildRemoved(ref(db, REF + "/users/" + store.currentUser.uid + "/plants"), plantRemovedInFirebase)
+  ];
 
   return;
+}
+
+export function enableFirebaseSync(store) {
+  if (!store.currentUser) {
+    // user should always be logged in when calling this,
+    // but check just in case.
+    console.log("can't enable Firebase sync when logged out");
+    return;
+  }
+
+  console.log("syncing Firebase ...");
+
+  function initStoreDataByFirebase(data) {
+    if (data.exists()) {
+      store.plants = Object.values(data.val());
+    }
+    else {
+      // user had no plant data saved
+      //console.log("no plant data for user in Firebase");
+    }
+
+    console.log("Firebase synced");
+
+    // set up sync after first load
+    updateFirebaseFromStore(store);
+    updateStoreFromFirebase(store);
+  }
+
+  // load data from Firebase, then set up sync
+  get(child(ref(db), REF + "/users/" + store.currentUser.uid + "/plants"))
+    .then(initStoreDataByFirebase)
+    .catch((error) => { console.error(error); });
+}
+
+export function disableFirebaseSync() {
+  console.log("desyncing Firebase ...");
+
+  unsubscribers.forEach(unsubscribe => unsubscribe());
+  unsubscribers = [];
+
+  console.log("Firebase desynced");
 }

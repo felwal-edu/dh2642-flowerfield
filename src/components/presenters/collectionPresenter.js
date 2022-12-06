d71a100dcf6d2718212208c7e64a1ac05af3e7ed
Linus Wallin <linuswallin@live.se>
2022-12-06 13:55:09 +0100

    created a table with flowers sorted in groups of genus. Co-authored-by: Matias Eriksson <matias.eriksson10@gmail.com>

--- src/components/presenters/collectionPresenter.js
+++ src/components/presenters/collectionPresenter.js
@@ -1,6 +1,7 @@
+import useFlowerStore from "@/data/flowerStore.js";
 import CollectionView from "../views/collectionView.js";
 
 export default
-function Collection(){
-    return <CollectionView/>;
+function Collection(props){
+    return <CollectionView plants={useFlowerStore().plants}/>;
 }

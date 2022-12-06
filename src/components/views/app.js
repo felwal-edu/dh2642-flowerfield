60f6af4c18f18fa1c7c8e2f1ec154c79254cacf9
Elliot <darthjr69@gmail.com>
2022-12-06 19:21:09 +0100

    Updated Upload to have overlay
    Added so app hides certain options
    Updated home view

--- src/components/views/app.js
+++ src/components/views/app.js
@@ -1,10 +1,27 @@
+import useFlowerStore from "@/data/flowerStore";
 import "../../main.css";
 
 export default
-function App(){
-    return (
-        <div>
-            <div id="nav">
+function App() {
+    let userStatus = useFlowerStore().currentUser;
+
+    function renderApp() {
+        useFlowerStore().$subscribe(function (mutation, state) {
+            if (mutation.events.key === "currentUser") {
+                // transform plant list to object with id as key
+                userStatus = mutation.events.newValue;
+            }
+        }.bind(this));
+
+        if (userStatus == undefined) {
+            return <div id="nav">
+                <router-link to="/">Home</router-link>
+                <router-link to="/about">About</router-link>
+                <router-link to="/login">Login</router-link>
+            </div>
+        }
+        else {
+            return <div id="nav">
                 <router-link to="/">Home</router-link>
                 <router-link to="/about">About</router-link>
                 <router-link to="/upload">Upload</router-link>
@@ -12,10 +29,17 @@ function App(){
                 <router-link to="/profile">Profile</router-link>
                 <router-link to="/login">Login</router-link>
             </div>
+        }
+
+
+    }
+
+    return (
+        <div>
+            {renderApp()}
             <div>
                 <router-view></router-view>
             </div>
-
         </div>
     )
 }

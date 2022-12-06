daee9e6c5fd9fc732346e18cc765de01bfdd50d4
Matias Eriksson <matiase@kth.se>
2022-12-06 21:32:31 +0100

    added vuetify to improve UI for loginview

--- src/components/views/loginView.js
+++ src/components/views/loginView.js
@@ -18,6 +18,7 @@ function LoginView(props) {
   }
 
   function emailInputChangeACB(evt) {
+    console.log(evt.target.value);
     props.onEmailChange(evt.target.value);
   }
 
@@ -27,12 +28,17 @@ function LoginView(props) {
 
   return (
     <div>
-      Email:
-      <input onChange={emailInputChangeACB} value=""></input>
-      Password:
-      <input onChange={passwordInputChangeACB} value=""></input>
-      <button onClick={signUpClickACB}>Sign up</button>
-      <button onClick={signInClickACB}>Sign in</button>
+      <v-app class="ma-8">
+        <v-card elevation="2" width="80%" color="green">
+          <v-col class="ma-8">
+            <v-row><v-card-text>Login/SignUp</v-card-text></v-row>
+            <v-row><v-text-field onChange={emailInputChangeACB}>Email: </v-text-field></v-row>
+            <v-row><v-text-field onChange={passwordInputChangeACB}>Password: </v-text-field></v-row>
+            <v-row class="mt-4"><v-btn onClick={signInClickACB}>Sign in</v-btn></v-row>
+            <v-row class="mt-4"><v-btn onClick={signUpClickACB}>Sign up</v-btn></v-row>
+          </v-col>
+        </v-card>
+      </v-app>
     </div>
   );
 }

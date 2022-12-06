c16e90762f3e832a71b28ec74a3d88fa416c0217
Elliot <darthjr69@gmail.com>
2022-12-06 13:19:30 +0100

    Vuetify linked

--- src/components/views/homeView.js
+++ src/components/views/homeView.js
@@ -1,6 +1,9 @@
 
 import useFlowerStore  from "@/data/flowerStore";
 import { examplePlant } from "@/data/network/plantIdServiceMock";
+import logopath from "@/assets/testlogo.png";
+import background from "@/assets/home_parallax.jpg";
+import "@/css/home.css"
 
 
 function HomeView() {
@@ -12,10 +15,21 @@ function HomeView() {
 
     return (
         <div>
-            <h1>Flower Field</h1>
-            <img src="../../assets/logo.png"></img>
+            <v-parallax height="600" src={background}>
+                <div class="d-flex flex-column fill-height justify-center align-center text-white">
+                    <div class="landing-page">
+                        <img class="logo" src={logopath}></img>
+                    </div>
+                </div>
+            </v-parallax>
+
+
             <button onclick={plantInfoACB}>add plants</button>
-        </div >
+            <v-card text="..."></v-card>
+            <div class="information-page">
+                This is a test to try this websites parallax effect! Scroll down and try it please!
+            </div>
+        </div>
     )
 }
 

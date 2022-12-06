278c3d7f546afba6e00333854f4327b76252f9a5
Elliot <darthjr69@gmail.com>
2022-12-06 16:55:33 +0100

    Upload reworked to older version
    Added simple authentication
    Change flowerStore identification

--- src/components/views/uploadView.js
+++ src/components/views/uploadView.js
@@ -1,9 +1,9 @@
 function UploadView(props) {
 
     function renderUploadView() {
-
         if (!props.imageLoaded) {
-            return <div class={props.dragareaActive == true ? "drag-area active" : "drag-area"} >
+            return <div class={props.dragareaActive == true ? "drag-area active" : "drag-area"}
+            onDragover={dragoverFileACB} onDragleave={dragleaveFileACB} onDrop={dropFileACB} >
                 <div class="icon">
 
                     <i class="fas"></i>
@@ -11,15 +11,30 @@ function UploadView(props) {
                 </div>
                 <span class="header">{props.dragareaActive == true ? "Release to Upload" : "Drag & Drop"}</span>
                 <span class="header">or <span class="browse-button" onClick={browseSpanClickACB}>browse</span></span>
-                <input type="file" hidden />
+                <input type="file" hidden onChange={inputFileChangeACB} />
 
                 <span class="tip-support">PNG, JPG, JPEG</span>
             </div>;
         }
         else {
-            return <div class="image-container">
-                <img src={props.fileURL} alt=""></img>
-            </div>
+            return <div>
+                    <div class="image-container">
+                        <img src={props.fileURL} alt=""></img>
+                    </div>
+                    <v-overlay v-model={props.overlay}>
+                    <v-card>
+                        <v-card-text>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        </v-card-text>
+                        <v-card-actions>
+                            <button onClick={testACB}>
+                                Close Dialog
+                            </button>
+                        </v-card-actions>
+                    </v-card>
+                    </v-overlay>
+                </div>
+
         }
     }
 
@@ -28,11 +43,33 @@ function UploadView(props) {
     }
 
     function uploadButtonClickACB() {
-        props.onUploadImageToAPI(props.fileURL)
+        props.onUploadImageToAPI();
     }
 
     function cancleButtonClickACB() {
         props.onAbortUpload();
+    }
+
+    function dragoverFileACB(evt) {
+        //evt.preventDefault();
+        props.onDragoverFile(evt);
+    }
+
+    function dragleaveFileACB(evt) {
+        props.onDragleaveFile();
+    }
+
+    function dropFileACB(evt) {
+        //evt.preventDefault();
+        props.onDropFile(evt);
+    }
+
+    function inputFileChangeACB(evt) {
+        props.onInputFileChange(evt);
+    }
+
+    function testACB(){
+        props.overlay = false;
     }
 
     return (

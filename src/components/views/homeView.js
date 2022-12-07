8dbeaa01de2db65cc6334164145b97467be0fba0
Elliot <darthjr69@gmail.com>
2022-12-07 02:14:08 +0100

    Art and Style for HomePage
    Co-Authored-By: Linus Wallin <73783914+LinusWallin@users.noreply.github.com>

--- src/components/views/homeView.js
+++ src/components/views/homeView.js
@@ -1,5 +1,9 @@
 import logopath from "@/assets/testlogo.png";
 import background from "@/assets/home_parallax.jpg";
+import home_flower from "@/assets/home_flower_background.png";
+import home_photo from "@/assets/home_photo.png";
+import home_growth from "@/assets/home_growth.png";
+import home_camera from "@/assets/home_camera.png";
 import "@/css/home.css"
 
 function HomeView(props) {
@@ -29,7 +33,56 @@ function HomeView(props) {
         </div>
       </v-parallax>
       <div class="information-page">
-        This is a test to try this websites parallax effect! Scroll down and try it please!
+        <v-container justify="center">
+            <v-col align-self="start" class="justify-center align-center pa-0" cols="12">
+                <v-row justify="center" class="mt-14">
+                <h1> A relaxing experience. </h1>
+                </v-row>
+                <v-row justify="center">
+                    <img class="home-photo" src={home_photo} alt="Logo"  />
+                    <p>Take pictures of flowers and plants - Flowerfield can automatically identify them, <br></br>
+                    providing you with new information about your surroundings.</p>
+                </v-row>
+            </v-col>
+        </v-container>
+        <v-container justify="center" class="mt-14">
+            <v-parallax height="500" src={home_flower}>
+                <v-col align-self="start" class="justify-center align-center pa-0" cols="12">
+                    <v-row justify="center" align="center" class="mt-14">
+                    <h1> A digital assortment. </h1>
+                    </v-row>
+                    <v-row justify="center" align="center" class="mt-14 pt-14">
+                        <p class="highlighted">Store your discoveries in a digital collection, hosted on Flowerfield. <br></br>
+                        View your progress and collect sets of flowers and plants.</p>
+                    </v-row>
+                </v-col>
+            </v-parallax>
+        </v-container>
+        <v-container justify="center">
+            <v-col align-self="start" class="justify-center align-center pa-0" cols="12">
+                <v-row justify="center" class="mt-14">
+                <h1> A journey of <span>growth.</span> </h1>
+                </v-row>
+                <v-row justify="center" align="center" class="mt-4">
+                    <img class="home-plant" src={home_growth} alt="Logo" />
+                    <p class="mt-10">As your collection grows, so do you! <br></br>
+                    Gain experience from collecting flowers, and grow your botanical expertise.</p>
+                </v-row>
+            </v-col>
+        </v-container>
+        <v-container class="information-end">
+            <v-col align-self="start" class="justify-center align-center pa-0" cols="12">
+                <v-row justify="center" class="mt-14">
+                <img class="home-camera" src={home_camera} alt="Logo" />
+                </v-row>
+                <v-row justify="center" class="mt-8 mb-8">
+                <h1> Capture the moment! </h1>
+                </v-row>
+                <v-row justify="center" class="mb-14">
+                    <p>Start using Flowerfield today for free</p>
+                </v-row>
+            </v-col>
+        </v-container>
       </div>
     </div>
   );

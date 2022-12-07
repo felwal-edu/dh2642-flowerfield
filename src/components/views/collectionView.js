94428612035dd799e91075e956381065e3d0950b
Linus Wallin <linuswallin@live.se>
2022-12-07 15:36:32 +0100

    Created expansion panels for collection view

--- src/components/views/collectionView.js
+++ src/components/views/collectionView.js
@@ -1,4 +1,5 @@
 import { sortPlants } from "@/utilities";
+import { capitalize } from "vue";
 
 function CollectionView(props) {
   return (
@@ -13,23 +14,32 @@ function CollectionView(props) {
 
 function renderCollection(plants) {
   function createRowsCB(plantItem) {
-    return <tr>{plantItem[1].map(createCollectionColCB)}</tr>;
+    console.log(plantItem);
+    return (
+      <v-expansion-panels focusable>
+        <v-expansion-panel title={capitalize(plantItem[0]) + " (" + plantItem[1].length + ")"}>
+          <v-expansion-panel-text>
+            <tr>{plantItem[1].map(createCollectionColCB)}</tr>
+          </v-expansion-panel-text>
+        </v-expansion-panel>
+      </v-expansion-panels>
+    );
   }
 
   function createCollectionColCB(plant) {
     return (
       <td>
-        <img src={plant.url} width="100" />
+        <v-card
+        max-width = "150">
+          <v-img src={plant.url} width="150" />
+          <v-card-title class="plant-name">{capitalize(plant.scientificName.split(" ")[1])}</v-card-title>
+        </v-card>
       </td>
     );
   }
 
   return (
-    <div>
-      <table>
-        <tbody>{Object.entries(sortPlants(plants)).map(createRowsCB)}</tbody>
-      </table>
-    </div>
+    <div>{Object.entries(sortPlants(plants)).map(createRowsCB)}</div>
   );
 }
 

f29d354c54f870ba075da4d060d846ae9d95c8b5
Linus Wallin <linuswallin@live.se>
2022-12-09 13:56:37 +0100

    created sorting for collections

--- src/views/collectionView.js
+++ src/views/collectionView.js
@@ -2,17 +2,37 @@ import { sortPlants } from "@/utils/plantUtils";
 import { capitalize } from "vue";
 
 function CollectionView(props) {
+  function onSortChangeACB(evt){
+    props.onSort(evt);
+  }
   return (
     <div>
       <div>
-        <h1>Your collection</h1>
+        <v-card>
+          <v-row>
+            <v-col>Your Collection</v-col>
+            <v-col
+              offset-md="5"
+            >
+              Sort:
+            </v-col>
+            <v-col>
+              <v-select
+                model-value={props.sort}
+                items={["Genus A-Z", "Genus Z-A"]}
+                onUpdate:modelValue={onSortChangeACB}
+              >
+              </v-select>
+            </v-col>
+          </v-row>
+        </v-card>
       </div>
-      <div>{renderCollection(props.plants, props.test)}</div>
+      <div>{renderCollection(props.plants, props.test, props.sort)}</div>
     </div>
   );
 }
 
-function renderCollection(plants, test) {
+function renderCollection(plants, test, order) {
   function createRowsCB(plantItem) {
     return (
       <v-expansion-panels focusable>
@@ -28,8 +48,8 @@ function renderCollection(plants, test) {
   function createCollectionColCB(plant) {
     return (
       <td>
-        <v-card max-width = "150" class="mx-3">
-          <v-img src={plant.url} width="150" />
+        <v-card max-width = "150" class="mx-3" >
+          <v-img src={plant.url} max-width="150" max-heigth="200" />
           <v-card-title class="plant-name">
             {capitalize(plant.scientificName.split(" ")[1])}
           </v-card-title>
@@ -38,9 +58,15 @@ function renderCollection(plants, test) {
     );
   }
 
-  return (
-    <div>{Object.entries(sortPlants(plants)).map(createRowsCB)}</div>
-  );
+  if (order === "Genus A-Z"){
+    return (
+      <div>{Object.entries(sortPlants(plants)).map(createRowsCB)}</div>
+    );
+  }else{
+    return (
+      <div>{Object.entries(sortPlants(plants)).reverse().map(createRowsCB)}</div>
+    );
+  }
 }
 
 export default CollectionView;

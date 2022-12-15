3390d35fd0e27c2b3c77a37946d0e84b10c725d6
Matias Eriksson <matiase@kth.se>
2022-12-15 16:58:49 +0100

    remade the look of the signupview

--- src/views/signupView.js
+++ src/views/signupView.js
@@ -1,3 +1,5 @@
+import background from "@/assets/signupbackground.jpg";
+
 function SignUpView(props) {
 
 
@@ -30,23 +32,35 @@ function SignUpView(props) {
     return (
 
         <div>
+
             <v-app class="ma-8" align="center">
-                <v-card class="mx-auto" elevation="2" width="100%" color="#b0d2cf">
-                    <v-col>
-                        <v-row justify="center"><v-title>Create account: </v-title></v-row>
-                        <v-row justify="center"><v-col sm="11"><v-text-field clearable onChange={emailInputChangeACB}>Email: </v-text-field></v-col></v-row>
-                        <v-row justify="center"><v-col sm="11"><v-text-field type="password" clearable onChange={passwordInputChangeACB}>Password: </v-text-field></v-col></v-row>
-                        <v-row justify="center"><v-col sm="11"><v-text-field type="password" clearable onChange={passwordCheckInputChangeACB}>Confirm password: </v-text-field></v-col></v-row>
-                        <v-row justify="center" class="mx-8"><v-btn onClick={signUpClickACB}>Sign up</v-btn></v-row>
-                        <v-row justify="center" class="ma-8"><v-btn onClick={goToLoginACB}>Already have an account? </v-btn></v-row>
-                    </v-col>
-                </v-card>
-                <v-snackbar v-model={props.snackbar} class="d-flex" color="#0d0963">
-                    {"Passwords don't match"}
-                </v-snackbar>
+                <v-parallax
+                    height="10vh"
+                    src={background}>
+                    <v-card class="mx-auto mt-12" elevation="4" width="35%" color="#000efe" justify="center">
+                        <v-col align-self="start" class="justify-center align-center pa-0" cols="12">
+                            <v-row justify="center">
+                                <v-card-title class="mt-12">
+                                    <h1 class="header-font-signup">Create an account:</h1>
+                                </v-card-title>
+                            </v-row>
+                            <v-row justify="center"><v-col sm="8"><v-text-field clearable onChange={emailInputChangeACB}>Email: </v-text-field></v-col></v-row>
+                            <v-row justify="center"><v-col sm="8"><v-text-field type="password" clearable onChange={passwordInputChangeACB}>Password: </v-text-field></v-col></v-row>
+                            <v-row justify="center"><v-col sm="8"><v-text-field type="password" clearable onChange={passwordCheckInputChangeACB}>Confirm password: </v-text-field></v-col></v-row>
+                            <v-row justify="center" class="mx-8"><v-btn onClick={signUpClickACB}>Sign up</v-btn></v-row>
+                            <v-row justify="center" class="my-4 mb-12">
+                                <p class="link-text">Already have an account? <span onClick={goToLoginACB}>Login</span></p>
+                            </v-row>
+                        </v-col>
+                    </v-card>
+                    <v-snackbar v-model={props.snackbar} class="d-flex" color="#0d0963">
+                        {"Passwords don't match"}
+                    </v-snackbar>
+                </v-parallax>
             </v-app>
 
-        </div>
+
+        </div >
     )
 }
 

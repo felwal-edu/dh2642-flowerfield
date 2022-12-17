8146ad19e1699649b08cecca7113603dddc10b07
Linus Wallin <linuswallin@live.se>
2022-12-17 11:58:14 +0100

    fixed search, so that the search can be cleared which removes the filter. Also updated the error for not finding plants (work in progress)

--- src/presenters/collectionPresenter.js
+++ src/presenters/collectionPresenter.js
@@ -2,10 +2,9 @@ import useFlowerStore from "@/store/flowerStore.js";
 import { examplePlantArray } from "@/network/plantIdExample.js";
 import CollectionView from "../views/collectionView.js";
 import DetailView from "@/views/detailView.js";
+import ErrorView from "@/views/errorView.js";
 import { watch } from "vue";
 import { waitingForUserToBeSignedIn } from "@/utils/userUtils.js";
-import resolvePromise from "@/utils/resolvePromise.js";
-import promiseNoData from "@/views/promiseNodata.js";
 import log from "@/utils/logUtils.js";
 
 const CollectionPresenter = {
@@ -60,20 +59,20 @@ const CollectionPresenter = {
 
     function updateQueryACB(query) {
       this.searchQuery = query;
+      if (this.searchQuery === "") {
+        this.searchStatus = false;
+      }
     }
 
     function searchACB() {
       log.d(this.searchQuery);
 
-      if (this.searchQuery !== "" && this.searchStatus === false) {
+      if (this.searchQuery !== "") {
         this.searchStatus = true;
-        this.icon = "mdi-close-circle";
         this.searchResult = useFlowerStore().searchPlants(this.searchQuery);
       }
       else {
         this.searchStatus = false;
-        this.icon = "mdi-magnify"
-        //reset model-value !??!?!?!?
       }
     }
 
@@ -81,32 +80,38 @@ const CollectionPresenter = {
       this.searchStatus = false;
     }
 
-    log.d("searchResultsPromiseState", this.searchResultsPromiseState);
-
-    return (
-      <div>
-        <CollectionView
-          plants={useFlowerStore().plants /*examplePlantArray*/}
-          sort={this.sortStatus}
-          searchStatus={this.searchStatus}
-          searchQuery={this.searchQuery}
-          searchQueryPlants={this.searchResult}
-          icon={this.icon}
-          username={this.username}
-          onSort={sortACB.bind(this)}
-          openPopup={openPopupACB.bind(this)}
-          updateQuery={updateQueryACB.bind(this)}
-          onSearch={searchACB.bind(this)}
-          resetSearch={resetSearchACB.bind(this)}
+    if (this.searchResult.length === 0 && this.searchStatus === true) {
+      return (
+        <ErrorView
+          goToHomePage={resetSearchACB.bind(this)}
         />
-        <DetailView
-          closePopup={closePopupACB.bind(this)}
-          onDelete={deletePlantACB.bind(this)}
-          currentPlant={this.selected}
-          overlay={this.popupStatus}
-        />
-      </div>
-    );
+      );
+    } else {
+      return (
+        <div>
+          <CollectionView
+            plants={useFlowerStore().plants}
+            sort={this.sortStatus}
+            searchStatus={this.searchStatus}
+            searchQuery={this.searchQuery}
+            searchQueryPlants={this.searchResult}
+            icon={this.icon}
+            username={this.username}
+            onSort={sortACB.bind(this)}
+            openPopup={openPopupACB.bind(this)}
+            updateQuery={updateQueryACB.bind(this)}
+            onSearch={searchACB.bind(this)}
+            resetSearch={resetSearchACB.bind(this)}
+          />
+          <DetailView
+            closePopup={closePopupACB.bind(this)}
+            onDelete={deletePlantACB.bind(this)}
+            currentPlant={this.selected}
+            overlay={this.popupStatus}
+          />
+        </div>
+      );
+    }
   },
 };
 

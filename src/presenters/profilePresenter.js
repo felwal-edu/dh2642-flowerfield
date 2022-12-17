363ca3aa44c4285493065393319d3d019a5349e7
Felix Wallin <nils.felix@gmail.com>
2022-12-17 11:21:17 +0100

    Delete account

--- src/presenters/profilePresenter.js
+++ src/presenters/profilePresenter.js
@@ -1,4 +1,4 @@
-import { signOutUser } from "@/persistence/firebaseAuth";
+import { removeUser, signOutUser } from "@/persistence/firebaseAuth";
 import useFlowerStore from "@/store/flowerStore";
 import ProfileView from "../views/profileView";
 import { rankDisplay, progressBarValue } from "@/utils/plantUtils";
@@ -6,6 +6,7 @@ import { watch } from "vue";
 import { waitingForUserToBeSignedIn } from "@/utils/userUtils";
 import "../css/profile.css"
 import log from "@/utils/logUtils";
+import { deleteUserData } from "@/persistence/firebaseModel";
 
 const ProfilePresenter = {
   data() {
@@ -28,8 +29,14 @@ const ProfilePresenter = {
 
     function signOutACB() {
       signOutUser();
-      // return to home
       this.$router.push({ name: "home" });
+    }
+
+    function deleteAccountACB() {
+      deleteUserData(useFlowerStore().currentUser);
+      removeUser();
+
+      this.$router.push({name: "home"});
     }
 
     function changeUsernameACB(evt, newName) {
@@ -43,6 +50,7 @@ const ProfilePresenter = {
       <ProfileView
         currentUser={useFlowerStore().currentUser}
         onSignOut={signOutACB.bind(this)}
+        onDeleteAccount={deleteAccountACB.bind(this)}
         amountPlants={useFlowerStore().plants.length}
         currentRank={rankDisplay(useFlowerStore().ranks, useFlowerStore().experience)}
         experienceBar={progressBarValue(useFlowerStore().ranks, useFlowerStore().experience)}

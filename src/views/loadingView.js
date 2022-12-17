5931a22fb3ec859f8312edb9e8af960576f7f413
Felix Wallin <nils.felix@gmail.com>
2022-12-17 17:16:54 +0100

    Center loading view

--- src/views/loadingView.js
+++ src/views/loadingView.js
@@ -1,6 +1,6 @@
 export default function LoadingView(props) {
   return (
-    <div justify="center" align="center" class="promsieNoData-loading ">
+    <div class="promsieNoData-loading optical-center">
       <img src="https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif?20151024034921" />
     </div>
   );

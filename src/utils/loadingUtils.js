aa0f7979605f6405ac15f3bc1cca242b822eeae0
Felix Wallin <nils.felix@gmail.com>
2022-12-17 14:14:00 +0100

    Rename loading flowers with camelCase

--- src/utils/loadingUtils.js
+++ src/utils/loadingUtils.js
@@ -1,17 +1,17 @@
 //TODO for-loop solution?
-import loading_flower1 from "@/assets/loading_icons/loading_icon_1.svg"
-import loading_flower2 from "@/assets/loading_icons/loading_icon_2.svg"
-import loading_flower3 from "@/assets/loading_icons/loading_icon_3.svg"
-import loading_flower4 from "@/assets/loading_icons/loading_icon_4.svg"
-import loading_flower5 from "@/assets/loading_icons/loading_icon_5.svg"
-import loading_flower6 from "@/assets/loading_icons/loading_icon_6.svg"
-import loading_flower7 from "@/assets/loading_icons/loading_icon_7.svg"
-import loading_flower8 from "@/assets/loading_icons/loading_icon_8.svg"
-import loading_flower9 from "@/assets/loading_icons/loading_icon_9.svg"
+import loadingFlower1 from "@/assets/loading_icons/loading_icon_1.svg"
+import loadingFlower2 from "@/assets/loading_icons/loading_icon_2.svg"
+import loadingFlower3 from "@/assets/loading_icons/loading_icon_3.svg"
+import loadingFlower4 from "@/assets/loading_icons/loading_icon_4.svg"
+import loadingFlower5 from "@/assets/loading_icons/loading_icon_5.svg"
+import loadingFlower6 from "@/assets/loading_icons/loading_icon_6.svg"
+import loadingFower7 from "@/assets/loading_icons/loading_icon_7.svg"
+import loadingFlower8 from "@/assets/loading_icons/loading_icon_8.svg"
+import loadingFlower9 from "@/assets/loading_icons/loading_icon_9.svg"
 
-const LOADING_IMAGES = [loading_flower1, loading_flower2, loading_flower3,
-                        loading_flower4, loading_flower5, loading_flower6,
-                        loading_flower7, loading_flower8, loading_flower9]
+const LOADING_IMAGES = [loadingFlower1, loadingFlower2, loadingFlower3,
+                        loadingFlower4, loadingFlower5, loadingFlower6,
+                        loadingFower7, loadingFlower8, loadingFlower9]
 
 function getRandomLoadingImage() {
     let item = LOADING_IMAGES[Math.floor(Math.random() * LOADING_IMAGES.length)]

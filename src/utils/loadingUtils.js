5774cc2f4f8be7c4de9bfc538acae5edb1e1c1d2
Felix Wallin <nils.felix@gmail.com>
2022-12-17 13:31:07 +0100

    Transform loading icons into svg
    + make them light grey

--- src/utils/loadingUtils.js
+++ src/utils/loadingUtils.js
@@ -1,13 +1,13 @@
 //TODO for-loop solution?
-import loading_flower1 from "@/assets/loading_icons/loading_icon_1.png"
-import loading_flower2 from "@/assets/loading_icons/loading_icon_2.png"
-import loading_flower3 from "@/assets/loading_icons/loading_icon_3.png"
-import loading_flower4 from "@/assets/loading_icons/loading_icon_4.png"
-import loading_flower5 from "@/assets/loading_icons/loading_icon_5.png"
-import loading_flower6 from "@/assets/loading_icons/loading_icon_6.png"
-import loading_flower7 from "@/assets/loading_icons/loading_icon_7.png"
-import loading_flower8 from "@/assets/loading_icons/loading_icon_8.png"
-import loading_flower9 from "@/assets/loading_icons/loading_icon_9.png"
+import loading_flower1 from "@/assets/loading_icons/loading_icon_1.svg"
+import loading_flower2 from "@/assets/loading_icons/loading_icon_2.svg"
+import loading_flower3 from "@/assets/loading_icons/loading_icon_3.svg"
+import loading_flower4 from "@/assets/loading_icons/loading_icon_4.svg"
+import loading_flower5 from "@/assets/loading_icons/loading_icon_5.svg"
+import loading_flower6 from "@/assets/loading_icons/loading_icon_6.svg"
+import loading_flower7 from "@/assets/loading_icons/loading_icon_7.svg"
+import loading_flower8 from "@/assets/loading_icons/loading_icon_8.svg"
+import loading_flower9 from "@/assets/loading_icons/loading_icon_9.svg"
 
 const LOADING_IMAGES = [loading_flower1, loading_flower2, loading_flower3,
                         loading_flower4, loading_flower5, loading_flower6,

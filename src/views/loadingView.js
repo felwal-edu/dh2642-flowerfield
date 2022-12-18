982b286cf1cda08c0feb6da666bcdf6a93e92ee2
Felix Wallin <nils.felix@gmail.com>
2022-12-18 11:04:56 +0100

    LoadingView: use veutify component

--- src/views/loadingView.js
+++ src/views/loadingView.js
@@ -1,7 +1,11 @@
 export default function LoadingView(props) {
   return (
-    <div class="optical-center">
-      <img src="https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif?20151024034921" />
+    <div class={"optical-center"}>
+      {
+        props.linear
+          ? <v-progress-linear indeterminate="true" color="light-green-darken-3" />
+          : <v-progress-circular indeterminate="true" color="light-green-darken-3" />
+      }
     </div>
   );
 }
